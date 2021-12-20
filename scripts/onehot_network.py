22e0bbd7b51ab0b0e17c5be1e6ab593ce2f47188
JulieMariaJ <79092882+JulieMariaJ@users.noreply.github.com>
2021-12-20 15:11:01 +0100

    Update onehot_network.py

--- scripts/onehot_network.py
+++ scripts/onehot_network.py
@@ -145,16 +145,23 @@ class One_Hot_Net(nn.Module):
         super(One_Hot_Net, self).__init__()  
         self.bn0 = nn.BatchNorm1d(channels)   
         self.conv1 = nn.Conv1d(in_channels=channels, out_channels=num_filters1, kernel_size=kernel_size_conv1, stride=stride_conv1, padding=padding_conv1)
+        torch.nn.init.kaiming_uniform_(self.conv1.weight)
         self.conv_bn1 = nn.BatchNorm1d(num_filters1) 
         self.conv_out_dim1 = compute_conv_dim(dim_size, kernel_size_conv1, padding_conv1, stride_conv1)
 
         self.conv2 = nn.Conv1d(in_channels=num_filters1, out_channels=num_filters2, kernel_size=kernel_size_conv2, stride=stride_conv2, padding=padding_conv2)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
         self.conv_bn2 = nn.BatchNorm1d(num_filters2) 
         self.conv_out_dim2 = compute_conv_dim(self.conv_out_dim1, kernel_size_conv2, padding_conv2, stride_conv2)
         
         self.rnn = nn.LSTM(input_size=num_filters2, hidden_size=hidden_size, num_layers=3, batch_first=True, bidirectional = True)
 
         self.fc1 = nn.Linear(2*hidden_size, num_classes)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        
+        #dropout
+        self.dropout_cnn = nn.Dropout(0.2)
+        self.dropout_linear = nn.Dropout(0.5) 
 
         self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
 
@@ -162,13 +169,16 @@ class One_Hot_Net(nn.Module):
         x = self.bn0(x)    
         x = self.pool(F.relu(self.conv1(x)))
         x = self.conv_bn1(x)
+        x = self.dropout_cnn(x)
 
         x = self.pool(F.relu(self.conv2(x)))
         x = self.conv_bn2(x)
+        x = self.dropout_cnn(x)
 
         x = x.transpose_(2, 1)
         x, (h, c) = self.rnn(x)
         cat = torch.cat((h[-2, :, :], h[-1, :, :]), dim=1)
+        cat = self.dropout_cnn(cat)
 
         x = torch.sigmoid(self.fc1(cat))
         return x
@@ -181,7 +191,7 @@ print(One_Hot_net)
 import torch.optim as optim
 
 criterion = nn.BCELoss()  
-optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay=1e-8)
+optimizer = optim.Adam(One_Hot_net.parameters(), lr=0.001, weight_decay=1e-8)
 
 ########## reset weights function ##########
 
@@ -196,64 +206,176 @@ def reset_weights(m):
     layer.reset_parameters()
 
 ########## Train network ##########
-num_epoch = 5  
+num_epoch = 100
 
-train_loss = []
-losses = []
-val_losses = []
+# For fold results
+train_results = {}
+val_results = {}
+best_nets, best_val_targets_list, best_val_preds_list, best_train_targets_list, best_train_preds_list = [], [], [], [], []
+best_train_preds_sig_list, best_val_preds_sig_list = [], []
+master_train_losses, master_val_losses = [], []
+master_train_aucs, master_val_aucs = [], []
 
-for epoch in range(num_epoch):  
-    running_loss = 0.0
-    val_loss = 0.0
+for fold, sample in enumerate(dataset_split):  
+  # Print
+    print(f'FOLD {fold}')
+    print('--------------------------------')
 
-    # train network
-    net.train() 
-    train_targets, train_preds = [], []
-    for i, (data, target) in enumerate(trainloader):
-        # get the inputs
-        inputs_train = data.float().detach().requires_grad_(True).cuda()
-        labels_train = torch.tensor(np.array(target), dtype=torch.float).unsqueeze(1).cuda()
+    train_set = dataset_split[:fold]+dataset_split[fold+1:]
+    train_set_flat = [item for sublist in train_set for item in sublist]
 
-        optimizer.zero_grad()
-
-        #output = baseline_net(inputs_train)
-        output = One_Hot_net(inputs_train)
-        
-        train_batch_loss = criterion(output, labels_train)
-        train_batch_loss.backward()
-        optimizer.step()
-         
-        preds = np.round(output.detach().cpu())
-        train_targets += list(np.array(labels_train.cpu()))
-        train_preds += list(preds.data.numpy().flatten())
-        running_loss += train_batch_loss.item()
-        
-    losses.append(running_loss / len(trainloader.dataset))
-
-    # evaluate
-    net.eval()
-    val_targets, val_preds = [], []
-    with torch.no_grad():
-        for i, (data, target) in enumerate(valloader):
-            # get the inputs
-            inputs_val = data.float().detach().cuda()
-            labels_val = target.float().detach().unsqueeze(1).cuda() #torch.tensor(np.array(target), dtype=torch.float).unsqueeze(1).cuda()
-            
-            #output = baseline_net(inputs_val)
-            output = One_Hot_net(inputs_val)
-            
-            val_batch_loss = criterion(output, labels_val)
-
-            val_predicts = np.round(output.detach().cpu())
-            val_targets += list(np.array(labels_val.cpu()))
-            val_preds += list(val_predicts.data.numpy().flatten())
-            val_loss += val_batch_loss.item()
+    # Define data loaders for training and testing data in this fold
+    trainloader_fold = torch.utils.data.DataLoader(
+                        train_set_flat, 
+                        batch_size=64, shuffle = True)
     
-    val_losses.append(val_loss / len(valloader.dataset))
+    x_train, y_train = next(iter(trainloader_fold))
+    
+    channels = x_train.shape[2]
+    dim_size = x_train.shape[1]
 
-    # display train loss per epoch
-    print("Training loss:", losses[-1], "\t Validation loss:", val_losses[-1])
-    print("Training MCC:", matthews_corrcoef(train_targets, train_preds), "\t Validation MCC:", matthews_corrcoef(val_targets, val_preds))
+    valloader_fold = torch.utils.data.DataLoader(
+                        dataset_split[fold],
+                        batch_size=64, shuffle = True)
+    
+    One_Hot_net.apply(reset_weights)
+    
+    no_epoch_improve = 0
+    max_val_auc = 0
+
+    train_loss = []
+    losses = []
+    val_losses = []
+    train_acc = []
+    valid_acc = []
+    minimum_val_loss = 1000
+
+    train_aucs = []
+    val_aucs = []
+
+    for epoch in range(num_epoch):  
+        print('epoch number:', epoch)
+        running_loss = 0.0
+        val_loss = 0.0
+
+        # train network
+        One_Hot_net.train()
+        train_targets, train_preds, train_preds_sig = [], [], []
+        for i, (data, target) in enumerate(trainloader_fold):
+            # get the inputs
+            x_train = data.float().detach().requires_grad_(True).cuda()
+            labels_train = torch.tensor(np.array(target), dtype=torch.float).unsqueeze(1).cuda()
+
+            optimizer.zero_grad()
+
+            output = One_Hot_net(x_train)
+
+            train_batch_loss = criterion(output, labels_train)
+            train_batch_loss.backward()
+            optimizer.step()
+            
+            preds = np.round(output.detach().cpu())
+            preds_sig = output.detach().cpu()
+            train_targets += list(np.array(labels_train.cpu()))
+            train_preds += list(preds.numpy().flatten())
+            train_preds_sig += list(preds_sig.numpy().flatten())
+            running_loss += train_batch_loss.item()
+
+        losses.append(running_loss / len(trainloader_fold.dataset))
+
+        # evaluate
+        One_Hot_net.eval()
+        val_targets, val_preds, val_preds_sig = [], [], []
+        with torch.no_grad():
+            for i, (data, target) in enumerate(valloader_fold):
+                # get the inputs
+                x_val = data.float().detach().requires_grad_(True).cuda()
+                labels_val = torch.tensor(np.array(target), dtype=torch.float).unsqueeze(1).cuda()
+                
+                output = One_Hot_net(x_val)
+                
+                val_batch_loss = criterion(output, labels_val)
+
+                val_predicts_sig = output.detach().cpu()
+                val_predicts = np.round(output.detach().cpu())
+                val_targets += list(np.array(labels_val.cpu()))
+                val_preds += list(val_predicts.numpy().flatten())
+                val_preds_sig += list(val_predicts_sig.numpy().flatten())
+                val_loss += val_batch_loss.item()
+        
+        val_losses.append(val_loss / len(valloader_fold.dataset))
+        print("validation loss:",val_loss/len(valloader_fold.dataset))
+ 
+        train_acc_cur = accuracy_score(train_targets, train_preds)
+        valid_acc_cur = accuracy_score(val_targets, val_preds)
+        
+        train_acc.append(train_acc_cur)
+        valid_acc.append(valid_acc_cur)
+
+        # AUC
+        fpr_train, tpr_train, threshold_train = metrics.roc_curve(train_targets, train_preds_sig)
+        roc_auc_train = metrics.auc(fpr_train, tpr_train)
+        fpr_val, tpr_val, threshold_val = metrics.roc_curve(val_targets, val_preds_sig)
+        roc_auc_val = metrics.auc(fpr_val, tpr_val)
+        train_aucs.append(roc_auc_train)
+        val_aucs.append(roc_auc_val)
+
+        # Early stopping
+        if (roc_auc_val / len(valloader_fold.dataset)) > max_val_auc:   #(val_loss / len(valloader_fold.dataset)) < min_val_loss:
+            print("Model found at epoch:", epoch)  # save model/predictions with lowest loss 
+            best_net = One_Hot_net
+            best_val_targets = val_targets
+            best_val_preds = val_preds
+            best_val_preds_sig = val_preds_sig
+            best_train_targets = train_targets
+            best_train_preds = train_preds
+            best_train_preds_sig = train_preds_sig
+            no_epoch_improve = 0
+            max_val_auc = (roc_auc_val / len(valloader_fold.dataset))     #min_val_loss = (val_loss / len(valloader_fold.dataset))
+        else:
+            no_epoch_improve +=1
+        if no_epoch_improve == 10:
+            print("Early stopping\n")
+            break
+
+    #save intermediate models 
+    best_nets.append(best_net)
+    torch.save(best_net, 'best_net{0}.pth'.format(fold))
+    best_val_targets_list.append(best_val_targets)
+    best_val_preds_list.append(best_val_preds)
+    best_train_targets_list.append(best_train_targets)
+    best_train_preds_list.append(best_train_preds)
+
+    best_train_preds_sig_list.append(best_train_preds_sig)
+    best_val_preds_sig_list.append(best_val_preds_sig)
+
+    # save losses per fold
+    master_train_losses.append(losses)
+    master_val_losses.append(val_losses)
+    
+    # save all AUCs in fold
+    master_train_aucs.append(train_aucs)
+    master_val_aucs.append(val_aucs)
+
+    # best AUC per fold 
+    fpr_train, tpr_train, threshold_train = metrics.roc_curve(best_train_targets, best_train_preds_sig)
+    roc_auc_train = metrics.auc(fpr_train, tpr_train)
+    train_results[fold + 1] = roc_auc_train
+    
+    fpr_val, tpr_val, threshold_val = metrics.roc_curve(best_val_targets, best_val_preds_sig)
+    roc_auc_val = metrics.auc(fpr_val, tpr_val)
+    val_results[fold + 1] = roc_auc_val
+  
+sum_train = 0.0
+sum_val = 0.0
+for key, value in train_results.items():
+  print(f'Fold {key}: {value} %')
+  sum_train += value
+print(f'Average: {sum_train/len(train_results.items())} %')
+for key, value in val_results.items():
+  print(f'Fold {key}: {value} %')
+  sum_val += value
+print(f'Average: {sum_val/len(val_results.items())} %')
 
 ########## define test data ##########
 
@@ -267,31 +389,79 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=batchsize, shuffle=
 
 ########## test network ##########
 
-test_probs, test_preds, test_targs = [], [], []
-with torch.no_grad():
-  for batch_idx, (data, target) in enumerate(testloader):
-      x_batch_test = data.float().detach().cuda()
-      y_batch_test = target.float().detach().unsqueeze(1).cuda()
-      
-      output = net(x_batch_test)
+#### concatenate models #####
 
-      test_batch_loss = criterion(output, y_batch_test)
-      test_predicts = np.round(output.detach().cpu())
-      test_targs += list(np.array(y_batch_test.cpu()))
-      test_preds += list(test_predicts.numpy().flatten())
+all_test_preds, all_test_targs = [], []
+all_test_preds_sig = []
+all_test_preds_mcc = []
 
-print("Test MCC:", matthews_corrcoef(test_targs, test_preds))
+for i in range(4):
+    cur_net = torch.load('best_net'+str(i)+'.pth')
+
+    test_preds, test_targs = [], []
+    test_preds_sig = []
+    test_preds_mcc = []
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(testloader):
+            x_test = data.float().detach().requires_grad_(True).cuda()
+            labels_test = torch.tensor(np.array(target), dtype=torch.float).unsqueeze(1).cuda()
+          
+            output = cur_net(x_test)
+
+            test_batch_loss = criterion(output, labels_test)
+
+            test_predicts = np.round(output.detach().cpu())
+            test_predicts_sig = output.detach().cpu()
+            test_pred_mcc = mcc_threshold(output.detach().cpu())
+            test_targs += list(np.array(labels_test.cpu()))
+            test_preds += list(test_predicts.numpy().flatten())
+            test_preds_sig += list(test_predicts_sig.numpy().flatten())
+            test_preds_mcc += list(test_pred_mcc)
+
+    all_test_preds.append(test_preds)
+    all_test_preds_sig.append(test_preds_sig)
+    all_test_preds_mcc.append(test_preds_mcc)
+    all_test_targs.append(test_targs)  
+
+all_test_preds = sum(all_test_preds, [])
+all_test_preds_sig = sum(all_test_preds_sig, [])
+all_test_preds_mcc = sum(all_test_preds_mcc, [])
+all_test_targs = sum(all_test_targs, [])
+
+fpr_test, tpr_test, threshold_test = metrics.roc_curve(all_test_targs, all_test_preds_sig)
+roc_auc_test = metrics.auc(fpr_test, tpr_test)
+print("Test AUC:", roc_auc_test)
+print("Test MCC:", matthews_corrcoef(all_test_targs, all_test_preds))
 
 ########## Performance ##########
 
-def plot_roc(targets, predictions):
+c = 3
+
+#Loss curve 
+epoch = np.arange(len(master_train_losses[c]))
+plt.figure()
+plt.plot(epoch, master_train_losses[c], 'r', epoch, master_val_losses[c], 'b')
+plt.legend(['Train loss','Validation loss'])
+plt.xlabel('Epochs'), plt.ylabel('Loss')
+plt.show
+
+#Loss curve based on AUC 
+epoch = np.arange(len(master_train_aucs[c]))
+plt.figure()
+plt.plot(epoch, master_train_aucs[c], 'r', epoch, master_val_aucs[c], 'b')
+plt.legend(['Train AUC','Validation AUC'])
+plt.xlabel('Epochs'), plt.ylabel('AUC')
+plt.show()
+
+# AUC plot 
+def plot_roc(targets, predictions, filename):
     # ROC
     fpr, tpr, threshold = metrics.roc_curve(targets, predictions)
     roc_auc = metrics.auc(fpr, tpr)
 
     # plot ROC
     plt.figure()
-    plt.title('Receiver Operating Characteristic')
+    plt.title('Validation ROC-curve')
     plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
     plt.legend(loc = 'lower right')
     plt.plot([0, 1], [0, 1],'r--')
@@ -299,9 +469,32 @@ def plot_roc(targets, predictions):
     plt.ylim([0, 1])
     plt.ylabel('True Positive Rate')
     plt.xlabel('False Positive Rate')
-    #plt.show()
+    plt.show()
 
+### concatenated ###
+best_train_targets_list_cat = sum(best_train_targets_list, [])
+best_train_preds_list_cat = sum(best_train_preds_list, [])
+best_val_targets_list_cat = sum(best_val_targets_list, [])
+best_val_preds_list_cat = sum(best_val_preds_list, [])
 
-plot_roc(train_targets, train_preds)
-plot_roc(val_targets, val_preds)
-plot_roc(test_targs, test_preds)
+best_train_preds_sig_list_cat = sum(best_train_preds_sig_list, [])
+best_val_preds_sig_list_cat = sum(best_val_preds_sig_list, [])
+
+plot_roc(best_train_targets_list_cat, best_train_preds_sig_list_cat, "train_AUC")
+plot_roc(best_val_targets_list_cat, best_val_preds_sig_list_cat, "val_AUC")
+
+# performance score for concatenated results
+
+# f1 score 
+print("F1 score of training", f1_score(best_train_targets_list_cat, best_train_preds_list_cat))
+print("F1 score of validation", f1_score(best_val_targets_list_cat, best_val_preds_list_cat))
+print("F1 score of test", f1_score(test_targs, test_preds))
+
+# confusion matrix
+print("Confusion matrix for training", confusion_matrix(best_train_targets_list_cat, best_train_preds_list_cat))
+print("Confusion matrix for validation", confusion_matrix(best_val_targets_list_cat, best_val_preds_list_cat))
+print("Confusion matrix for test", confusion_matrix(test_targs, test_preds))
+
+# MCC 
+print("MCC for training", matthews_corrcoef(best_train_targets_list_cat, best_train_preds_list_cat))
+print("MCC for val", matthews_corrcoef(best_val_targets_list_cat, best_val_preds_list_cat))
